825828a5a71382046085e6b3cdfbc8a238e0476d
yp.kkp.xiao <yp.kkp.xiao@87ab913b-cbde-89c0-4e10-aaae81ba3aa6>
2013-08-09 00:59:20 +0000

    commit chrome_item_plugin

---
+++ branches/chrome_item_plugin/switch.js
@@ -0,0 +1,34 @@
+﻿function doSwitch() {
+	if (window.localStorage) {
+		localStorage.switchStatus = ("OFF" == $("#switch").attr("alt")) ? "ON" : "OFF";
+		$("#switch").attr("alt", localStorage.switchStatus);
+		$("#switch").attr("src", "./"+localStorage.switchStatus+".jpg");
+	} else {
+		var background = chrome.extension.getBackgroundPage();
+		background.switchStatus = ("OFF" == $("#switch").attr("alt")) ? "ON" : "OFF";
+		$("#switch").attr("alt", background.switchStatus);
+		$("#switch").attr("src", "./"+background.switchStatus+".jpg");
+	}
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  $("#switch").click(function() {
+	doSwitch();
+  });
+});
+
+function initStatus() {
+	if (window.localStorage) {
+		localStorage.switchStatus = localStorage.switchStatus ? localStorage.switchStatus : "OFF";
+		$("#switch").attr("alt", localStorage.switchStatus);
+		$("#switch").attr("src", "./"+localStorage.switchStatus+".jpg");
+	} else {
+		var background = chrome.extension.getBackgroundPage();
+		$("#switch").attr("alt", background.switchStatus);
+		$("#switch").attr("src", "./"+background.switchStatus+".jpg");
+	}
+}
+
+window.addEventListener('load', initStatus);
