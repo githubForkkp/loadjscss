2aea6349809205a5140c78c3095a62d2bf624fad
yp.kkp.xiao <yp.kkp.xiao@87ab913b-cbde-89c0-4e10-aaae81ba3aa6>
2013-08-12 09:35:41 +0000

    up

--- branches/chrome_item_plugin/content_script.js
+++ branches/chrome_item_plugin/content_script.js
@@ -15,21 +15,51 @@ function fetchItemData(callback) {
   var url = "http://bigblue.tmall.net:8088/client/getItems.htm";
   xhr.open("POST", url, true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.send("itemIds=" + genQueryStr());
+  xhr.send("itemIds=" + JSON.stringify(itemIds));
 };
 
+function getItemStatus(callback){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(data) {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var data = JSON.parse(xhr.responseText);
+        callback(data);
+      } else {
+        callback(null);
+      }
+    }
+  }
+
+  var url = "http://campaign.admin.taobao.org/campaign/tools/QueryItems.do?itemIdsString="+itemIds.toString();
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send();
+}
+//商品状态
+arrstatus = new Array();
+itemStatus = {
+    "0":"正常",
+    "1":"正常",
+    "-1":"用户删除",
+    "-2":"用户下架",
+    "-3":"小二下架",
+    "-4":"小二删除",
+    "-5":"从未上架",
+    "-9":"CC",
+    "状态查询异常":"查询状态异常"
+}
 var items = new Array();
 function genQueryStr() {
   itemIds = new Array();
-  var aRegx = /^http:\/\/(detail|item)\.(tmall|taobao)\.com\/item\.htm\?.*$/i;
+  var aRegx = /^http:\/\/(detail|item)\.(tmall|taobao)\.com\/|\/\/item\.htm\?.*$/i;
   var links = document.querySelectorAll("a");
   itemLinks = [];
 
   for (var i = 0; i < links.length; i++) {
     var aObj = links[i];
     if ($(aObj).attr("href") && aRegx.test($(aObj).attr("href")) && $(aObj).find("img") && $(aObj).find("img").length > 0) {
-
-      itemLinks.push(links[i]);
+    itemLinks.push(links[i]);
 	  var paramStr = $(aObj).attr("href").split("?");
 	  if (paramStr.length == 2) {
 	    var item = new Object();
@@ -69,8 +99,8 @@ function genItemHtml(data) {
 		  + "<p style='padding:0 5px;height:auto;' title='" + getValue(info, "title") + "'>名称：" + getValue(info, "title") + "</p>"
 		  + "<p style='padding:0 5px;height:auto;'>原价：" + getValue(info, "defaultItemPrice") + "元</p>"
 		  + "<p style='padding:0 5px;height:auto;'>专柜价：" + ((getValue(info, "tagPrice") != "")? getValue(info, "tagPrice") + "元" : "无") + "</p>"
-		  + "<p style='padding:0 5px;height:auto;'>状态：" + (!getValue(info, "isDownShelf") ? ("出售(库存" + parseInt(getValue(info, "icTotalQuantity")) + "件)") :
-			"<b style='color:red;'>下架</b>") +"</p>" + "</div>";
+		  + "<p style='padding:0 5px;height:auto;'>状态：" + ((arrstatus[i] == "0" || arrstatus[i] == "1") ?"正常":
+      "<b style='color:red;'>"+itemStatus[arrstatus[i]]+"</b>" )+"</p>" + "</div>";
 		$(item.obj).before(html);
 	  }
 	}
@@ -78,6 +108,24 @@ function genItemHtml(data) {
     console.log("不是的");
   }
 }
+
+function getItems(data){
+  if(data.totalStatus == "success"){
+    Results = data.itemQueryResults;
+    for (var i = 0; i < Results.length; i++) {
+        if(Results[i].statusMsg == "查询成功"){
+          arrstatus.push(Results[i].itemStatus)
+        }
+        else{
+          arrstatus.push("状态查询异常")
+        }
+    }
+  }else{
+    console.log("查询接口异常！")
+  }
+    fetchItemData(genItemHtml);
+}
+
 
 function getValue(object, key) {
 	for (var i in object) { 
@@ -94,15 +142,16 @@ function checkUrl(){
 	var links = document.querySelectorAll("a");
 	//匹配重复
 	var regx0 = /(http|http:\/\/){2,}/;
+  var regx1 = /(http|http:\/\/){1,}$/
 	//匹配缺少
-	var regx1 = /(http){1}/;
+	var regx2 = /(http){1}/;
 	for (var i = 0;i <= links.length-1;i++) {
 		var li = $(links[i]).attr("href");
 		
-		if (regx0.test(li)) {
+		if (regx0.test(li)||regx1.test(li)) {
 			setTips(links[i],msg);
 		}
-		else if(!regx1.test(li)){
+		else if(!regx2.test(li)){
 			if(/^#/.test(li)||/^(javascript)/.test(li)){
 				continue;
 			}
@@ -113,20 +162,23 @@ function checkUrl(){
 
 //检查重复id
 function checkId(){
-	genQueryStr();
-	var items=itemIds.sort();
- 	for(var i in itemIds){  
- 		if (items[i] == items[i+1]){
-  			setTips(itemLinks[i],"这里商品id重复,请检查！");
- 		}  
   
+ 	for(var i in itemIds){
+    for(var j in itemIds) {
+      if(i == j){
+        continue;
+      }
+ 		   if (items[i].id == items[j].id){
+  			   setTips(itemLinks[i],"此商品id有重复,请检查！");
+ 		     }  
+  }
 	}  
 }
 
 //气泡
 function setTips(node,msg){
     $(node).qtip({
-                content: msg + $(node).attr("href"),
+                content: msg + $(node).attr('href'),
                 show: {
                   when: false, 
                   ready: true
@@ -148,12 +200,12 @@ function setTips(node,msg){
 
 chrome.extension.sendMessage({greeting: "hello"}, function(response) {
   if (response.switchStatus == "ON") {
-    //fetchItemData(genItemHtml);
-    console.info(response)
+    genQueryStr();
+    getItemStatus(getItems);
     if(response.checkUrlStatus == "true"){
     	checkUrl();
     }
-    else if(response.checkIdStatus == "true"){
+    if(response.checkIdStatus == "true"){
     	checkId();
     }
    
